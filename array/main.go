f67a7b14a91602b3252d2489bb5274f9c7b9a957
MARQUEZ Laura Ferreira <lmarquez@ext.worten.pt>
2024-01-30 23:11:13 +0000

    left rotation

--- array/main.go
+++ array/main.go
@@ -1,75 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// ex 1 - reverse
-	// a := []int32{4, 3, 2, 1}
+	a := []int32{4, 3, 2, 1}
 
-	// var b []int32
+	var b []int32
 
-	// for i := len(a) - 1; i >= 0; i-- {
-	// 	b = append(b, a[i])
-	// }
-	// fmt.Println(b)
+	for i := len(a) - 1; i >= 0; i-- {
+		b = append(b, a[i])
+	}
+	fmt.Println(b)
 
 	// ex 2 - hourglass
-	// arr := [][]int32{
+	arr1 := [][]int32{
 
-	// 	// 19
-	// 	// {1, 1, 1, 0, 0, 0},
-	// 	// {0, 1, 0, 0, 0, 0},
-	// 	// {1, 1, 1, 0, 0, 0},
-	// 	// {0, 0, 2, 4, 4, 0},
-	// 	// {0, 0, 0, 2, 0, 0},
-	// 	// {0, 0, 1, 2, 4, 0},
+		// 19
+		// {1, 1, 1, 0, 0, 0},
+		// {0, 1, 0, 0, 0, 0},
+		// {1, 1, 1, 0, 0, 0},
+		// {0, 0, 2, 4, 4, 0},
+		// {0, 0, 0, 2, 0, 0},
+		// {0, 0, 1, 2, 4, 0},
 
-	// 	// -6
-	// 	// {-1, -1, 0, -9, -2, -2},
-	// 	// {-2, -1, -6, -8, -2, -5},
-	// 	// {-1, -1, -1, -2, -3, -4},
-	// 	// {-1, -9, -2, -4, -4, -5},
-	// 	// {-7, -3, -3, -2, -9, -9},
-	// 	// {-1, -3, -1, -2, -4, -5},
+		// -6
+		// {-1, -1, 0, -9, -2, -2},
+		// {-2, -1, -6, -8, -2, -5},
+		// {-1, -1, -1, -2, -3, -4},
+		// {-1, -9, -2, -4, -4, -5},
+		// {-7, -3, -3, -2, -9, -9},
+		// {-1, -3, -1, -2, -4, -5},
 
-	// 	// -19
-	// 	{0, -4, -6, 0, -7, -6},
-	// 	{-1, -2, -6, -8, -3, -1},
-	// 	{-8, -4, -2, -8, -8, -6},
-	// 	{-3, -1, -2, -5, -7, -4},
-	// 	{-3, -5, -3, -6, -6, -6},
-	// 	{-3, -6, 0, -8, -6, -7},
-	// }
+		// -19
+		{0, -4, -6, 0, -7, -6},
+		{-1, -2, -6, -8, -3, -1},
+		{-8, -4, -2, -8, -8, -6},
+		{-3, -1, -2, -5, -7, -4},
+		{-3, -5, -3, -6, -6, -6},
+		{-3, -6, 0, -8, -6, -7},
+	}
 
-	// hourglasses := make([]int32, 0)
+	hourglasses := make([]int32, 0)
 
-	// maxHourglass := math.MinInt32
-	// maxHourglass32 := int32(maxHourglass)
+	maxHourglass := math.MinInt32
+	maxHourglass32 := int32(maxHourglass)
 
-	// for row := 0; row <= 3; row++ {
-	// 	for col := 0; col <= 3; col++ {
-	// 		var sum int32
-	// 		sum = 0
+	for row := 0; row <= 3; row++ {
+		for col := 0; col <= 3; col++ {
+			var sum int32
+			sum = 0
 
-	// 		sum += arr[row][col]
-	// 		sum += arr[row][col+1]
-	// 		sum += arr[row][col+2]
-	// 		sum += arr[row+1][col+1]
-	// 		sum += arr[row+2][col]
-	// 		sum += arr[row+2][col+1]
-	// 		sum += arr[row+2][col+2]
+			sum += arr1[row][col]
+			sum += arr1[row][col+1]
+			sum += arr1[row][col+2]
+			sum += arr1[row+1][col+1]
+			sum += arr1[row+2][col]
+			sum += arr1[row+2][col+1]
+			sum += arr1[row+2][col+2]
 
-	// 		hourglasses = append(hourglasses, sum)
+			hourglasses = append(hourglasses, sum)
 
-	// 		if sum > maxHourglass32 {
-	// 			maxHourglass32 = sum
-	// 		}
-	// 	}
-	// }
+			if sum > maxHourglass32 {
+				maxHourglass32 = sum
+			}
+		}
+	}
 
-	// // Imprimir os valores calculados
-	// fmt.Println("Hourglasses:", hourglasses)
-	// fmt.Println("Valor máximo de Hourglasses:", maxHourglass32)
+	// Imprimir os valores calculados
+	fmt.Println("Hourglasses:", hourglasses)
+	fmt.Println("Valor máximo de Hourglasses:", maxHourglass32)
 
 	// ex 3 - dynamicArray
 	queries := [][]int32{
@@ -108,5 +111,30 @@ func main() {
 		}
 	}
 	fmt.Println(answerArr)
+
+	// ex 4 - left rotation
+	d := 3
+	arrEx := []int{1, 2, 3, 4, 5}
+
+	// 1
+	left := arrEx[:d]
+	right := arrEx[d:]
+	arrRotation := append(right, left...)
+	fmt.Println(arrRotation)
+
+	// 2
+	var moveArr []int
+	var newLeft []int
+	for i := 0; i < len(arrEx); i++ {
+		if arrEx[i] <= d {
+			moveArr = append(moveArr, arrEx[i])
+		} else {
+			newLeft = append(newLeft, arrEx[i])
+		}
+	}
+	concatArr := append(newLeft, moveArr...)
+	fmt.Println(concatArr)
+
+	// ex 5 -
 
 }
